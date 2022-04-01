6fcfa6250cfebca098da73947ecb6cdfe183428f
MayurHajare2103 <88882367+MayurHajare2103@users.noreply.github.com>
2022-04-01 22:41:47 +0530

    chapter 1 c language code

---
+++ input.c
@@ -0,0 +1,48 @@
+//addition of two no
+
+#include <stdio.h>
+ 
+int main()
+{
+    int x, y;
+    printf("Enter the first no :");
+    scanf("%d", &x);
+
+    printf("\nEnter the second no : ");
+    scanf("%d", &y);
+
+    printf("\n The sum of no. is %d", x + y);
+    return 0;
+
+}
+
+
+// taking input from users contry details with using c variables and inputs.
+
+/*
+#include <stdio.h>
+
+int main() {
+    char country_name[] ;
+   // char capital[10] ;
+  //  int total_states ;
+  //  int population ;
+ //   float education_rate;
+ //   char courency ;
+    
+    printf ("Enter the country name , capital , total states , population ,education rate and  courency of your contry : ");
+    
+    scanf("%s",country_name );
+  //  scanf("%s",capital  );
+  //  scanf("%d",total_states );
+   // scanf("%d",population  );
+  //  scanf("%f",education_rate  );
+ //   scanf("%c",courency  );
+    
+    
+    printf ("\n \n My country is %s and capital is %s. And no. of states in %s is %d. population of the country is  %d and education rate is %d.Currency of country is %c", country_name  //, capital ,country_name , total_states , population ,education_rate , courency
+     );
+    
+    return 0;
+    }*/
+    
